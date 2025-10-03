64e68c1bde8e3291eb56384329991c5398177d14
adisingh0027 <135611028+adisingh0027@users.noreply.github.com>
2025-10-03 16:28:49 +0530

    test.py

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyBqmcOUimDaB6-CmCGwlY9AoqJlEinh7-Y")
+client = genai.Client(api_key="GOOGLE_API_KEY")
 
 
 
 # List available models
 for model in client.models.list():  # Pager object is iterable
     print(model.name)
+
